ea99cbc1de3552dce28317ca3a1993277183c8ac
krishna <in.krishna@gmail.com>
2025-11-11 21:32:03 -0500

    Move nsepulse-spring to separate repository (Stock-Tracker-Spring)

--- nsepulse-spring/frontend/src/App.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders nse pulse title', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/NSE Pulse/i);
-  expect(linkElement).toBeInTheDocument();
-});
